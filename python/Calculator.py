7aa306571bc4d7e5d2d3029eb229e5d38a09f94a
yvaine77 <156685369+yvaine77@users.noreply.github.com>
2024-01-21 22:37:39 +0000

    Add files via upload

---
+++ python/Calculator.py
@@ -0,0 +1,45 @@
+def add(num1, num2):
+    return num1 + num2
+
+def subtract(num1, num2):
+    return num1 - num2
+
+def multiply(num1, num2):
+    return num1 * num2
+
+def divide(num1, num2):
+    if num2 == 0:
+        return "Error: Division by zero is not allowed"
+    else:
+        return num1 / num2
+    
+def calculator():
+    print("Select an operation:")
+    print("1. Add")
+    print("2. Subtract")
+    print("3. Multiply")
+    print("4. Divide")
+    print("5.Exit")
+
+    while True:
+        choice = input("Enter choice (1/2/3/4): ")
+        if choice in ["1", "2", "3", "4"]:
+            num1=float(input("Enter first number: "))
+            num2=float(input("Enter second number: "))
+            
+            if choice == "1":
+                print(add(num1, num2))
+            elif choice == "2": 
+                print(subtract(num1, num2))
+            elif choice == "3": 
+                print(multiply(num1, num2))
+            elif choice == "4": 
+                print(divide(num1, num2))
+        
+        elif choice == "5.":
+            break
+        else:
+            print("Invalid input. Please try again.")
+
+    
+calculator()
